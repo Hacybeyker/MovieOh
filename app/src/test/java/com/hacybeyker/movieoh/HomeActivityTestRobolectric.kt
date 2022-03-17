229d7aa64617e94586089a2c2024eda71558e45b
Carlos Osorio <hacybeyker@gmail.com>
2022-03-17 09:24:07 -0500

    Feature splash (#1)
    * Se arealizaron modificaciones:
    - se modifico el icono del app.
    - se implemento el splash.
    - se agrego archivos para excluir en jacoco.
    - se agrego la dependencia de lottie.
    
    * Se modifico el dependencies
    * Se agrego test para SplashActivity
    * Se agrego test para NetworkStatus

--- app/src/test/java/com/hacybeyker/movieoh/HomeActivityTestRobolectric.kt
+++ app/src/test/java/com/hacybeyker/movieoh/HomeActivityTestRobolectric.kt
@@ -4,17 +4,17 @@ import android.widget.TextView
 import androidx.lifecycle.Lifecycle
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.hacybeyker.movieoh.ui.home.MainActivity
+import com.hacybeyker.movieoh.ui.home.HomeActivity
 import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class MainActivityTestRobolectric {
+class HomeActivityTestRobolectric {
 
     @get:Rule
-    val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+    val activityScenarioRule = ActivityScenarioRule(HomeActivity::class.java)
 
     @Test
     fun showActivityDefault() {
