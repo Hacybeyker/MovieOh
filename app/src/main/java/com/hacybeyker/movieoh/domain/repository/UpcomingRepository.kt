cf806be40e695cad5417d3e65c8650862afa2f7f
Carlos Osorio <hacybeyker@gmail.com>
2022-04-25 08:58:20 -0500

    se agregaron las variables para local.properties por cada steps para githubactions

--- app/src/main/java/com/hacybeyker/movieoh/domain/repository/UpcomingRepository.kt
+++ app/src/main/java/com/hacybeyker/movieoh/domain/repository/UpcomingRepository.kt
@@ -3,6 +3,5 @@ package com.hacybeyker.movieoh.domain.repository
 import com.hacybeyker.movieoh.domain.entity.MovieEntity
 
 interface UpcomingRepository {
-
     suspend fun fetchUpcoming(page: Int): List<MovieEntity>
 }
--- app/src/main/java/com/hacybeyker/movieoh/domain/usecase/TrendingUseCase.kt
+++ app/src/main/java/com/hacybeyker/movieoh/domain/usecase/TrendingUseCase.kt
@@ -4,7 +4,9 @@ import com.hacybeyker.movieoh.domain.entity.MovieEntity
 import com.hacybeyker.movieoh.domain.repository.TrendingRepository
 import javax.inject.Inject
 
-class TrendingUseCase @Inject constructor(private val repository: TrendingRepository) {
+class TrendingUseCase @Inject constructor(
+    private val repository: TrendingRepository
+) {
 
     suspend fun fetchTrendingMovie(page: Int): List<MovieEntity> {
         return repository.fetchTrendingMovie(page)
