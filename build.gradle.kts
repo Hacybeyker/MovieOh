25615a441b5b77bd39516e6141f3f9013ae10a15
Carlos Osorio <hacybeyker@gmail.com>
2022-06-06 10:47:00 -0500

    WIP: - Se modifico el gradle a 7.2.1. - Se agrego mocks para debug. - Se implemento modo dark. - Se agregaromn stylos y themas.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
         }
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.3")
+        classpath("com.android.tools.build:gradle:7.2.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.39.1")
         classpath("org.jacoco:org.jacoco.core:0.8.7")
