e477d44efcbcd3adc2f0267694daf31ec7782939
Carlos Osorio <hacybeyker@gmail.com>
2022-07-06 22:38:09 -0500

    enhancement project (#14)
    * update yml files
    * update lint action yml
    * update yml and change version name app

--- buildSrc/src/main/kotlin/ConfigureApp.kt
+++ buildSrc/src/main/kotlin/ConfigureApp.kt
@@ -1,8 +1,7 @@
 object ConfigureApp {
-    const val urlRepoDependencies = "https://maven.pkg.github.com/Hacybeyker/your_path_module"
     const val organization = "hacybeyker"
     const val projectName = "app-movieoh-android"
     const val applicationId = "com.hacybeyker.movieoh"
-    const val versionCode = 3
-    const val versionName = "3.0.0"
+    const val versionCode = 4
+    const val versionName = "4.0.0"
 }
